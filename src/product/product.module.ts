20f78d45157b8797662d0c4bc08cd52d0dc5fdca
sebasGZA <sebassga34@gmail.com>
2024-11-15 16:48:07 -0500

    fix: remove useless imports

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
 import { ProductRepository } from './product.repository';
 import { CategoryModule } from 'src/category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product]), CategoryModule],
+  imports: [CategoryModule],
   controllers: [ProductController],
   providers: [ProductService, ProductRepository],
   exports: [ProductService],
